f3e7b26b2b3cf6bc810176497507a785f70082dd
Hossein Hout Ganjabadi <ho3ein1ted@gmail.com>
2025-12-10 20:16:58 +0330

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,15 @@
-const CACHE_NAME = 'taladose-v4-final'; // Cache version updated!
+const CACHE_NAME = 'taladose-v5-stable'; // Cache version updated!
 const urlsToCache = [
   '/',
   '/index.html',
   '/css/style.css',
   '/js/main.js',
-  'https://raw.githubusercontent.com/Ganjabady/TalaDose1/refs/heads/main/taladoseicon250.png',
+  'https://raw.githubusercontent.com/Ganjabady/TalaDose/5558bfd49b93a40b376ff43a2d3673873f04ccdf/fav.png',
   'https://fonts.googleapis.com/css2?family=Vazirmatn:wght@400;500;600;700;800&display=swap'
 ];
 
-// Install a service worker
 self.addEventListener('install', event => {
-  self.skipWaiting(); // Force the new service worker to become active
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
@@ -20,21 +19,13 @@ self.addEventListener('install', event => {
   );
 });
 
-// Cache and return requests
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response; // Serve from cache
-        }
-        return fetch(event.request); // Fetch from network
-      }
-    )
+      .then(response => response || fetch(event.request))
   );
 });
 
-// Update a service worker and remove old caches
 self.addEventListener('activate', event => {
   const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
@@ -47,7 +38,6 @@ self.addEventListener('activate', event => {
           }
         })
       );
-    })
+    }).then(() => self.clients.claim())
   );
-  return self.clients.claim();
 });
