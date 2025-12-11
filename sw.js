c89dc5ad1b98859beeee5336c2a91ef56a43576a
Hossein Hout Ganjabadi <ho3ein1ted@gmail.com>
2025-12-11 13:29:40 +0330

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'taladose-v13-final-fix-v7'; // Cache version updated!
+const CACHE_NAME = 'taladose-v13-final-fix-v8'; // Cache version updated!
 const urlsToCache = [
   '/',
   '/index.html',
